65dd947cf0bf88a9758e2392a6d43b4e035afa91
Haohui Que <quehaohui@dp.tech>
2023-04-25 11:46:56 +0800

    update LiCl_DP_Tutoeial_1.ipynb

---
+++ Examples/LiCl_DP_Tutorial_Example/chapter3/01.train/lcurve.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = np.genfromtxt("lcurve.out", names=True)
+for name in data.dtype.names[1:-1]:
+    plt.plot(data['step'], data[name], label=name)
+plt.legend()
+plt.xlabel('Step')
+plt.ylabel('Loss')
+plt.yscale('log')
+plt.savefig('./lcurve.png',dpi=300)
